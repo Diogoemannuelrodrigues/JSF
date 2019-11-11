4a566f85f6330e298aeedd084986183ec391df9f
Diogo Emannuel Ferreira Rodrigues <diogo.rodrigues@basis.com.br>
2019-11-11 19:53:52 -0300

    Adicionando o projeto jsf da algaworks

---
+++ jsf-algamoney/src/com/jsf/OlaMundo.java
@@ -0,0 +1,32 @@
+package com.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaMundo {
+
+	private String nome;
+
+	private String nomeAlterado;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void tranformaEmMaiusculo() {
+		this.setNomeAlterado(this.getNome().toUpperCase());
+	}
+
+	public String getNomeAlterado() {
+		return nomeAlterado;
+	}
+
+	public void setNomeAlterado(String nomeAlterado) {
+		this.nomeAlterado = nomeAlterado;
+	}
+
+}
