a3d99fbc1202a145809fc9ee6289f0fea32096f3
Diogo Emannuel Ferreira Rodrigues <diogo.rodrigues@basis.com.br>
2019-11-12 18:52:41 -0300

    Adicionando metodo de exclao do objeto da lista

--- jsf-algamoney/src/br/com/manageBean/GestaoFesta.java
+++ jsf-algamoney/src/br/com/manageBean/GestaoFesta.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean
 @SessionScoped
@@ -12,17 +13,50 @@ public class GestaoFesta {
 
 	private Festa festa;
 	private List<Festa> festas;
+	private Festa festaSelecionado;
+
+	public Festa getFestaSelecionado() {
+		return festaSelecionado;
+	}
+
+	public void setFestaSelecionado(Festa festaSelecionado) {
+		this.festaSelecionado = festaSelecionado;
+	}
 
 	public GestaoFesta() {
 		this.festas = new ArrayList<Festa>();
 		this.festa = new Festa();
 	}
-	
+
 	public String obtemAjuda() {
-		if(this.festas.isEmpty())
+		if (this.festas.isEmpty())
 			return "ajuda?faces-redirect=true";
-			else
+		else
 			return "temAlgo?faces-redirect=true";
+	}
+
+	public void verificaInclusao(ActionEvent e) {
+
+		if ((this.festa.getEmail().equals("")) && this.festa.getIdade().equals("") 
+				&& this.festa.getNome().equals("")) {
+			this.festa.setEmail("Sem dado cadastrado");
+			this.festa.setNome("Sem dado cadastrado");
+			this.festa.setIdade("Sem dado cadastrado");
+		}
+		if (this.festa.getEmail().equals("")) {
+			this.festa.setEmail("Sem dado cadastrado");
+		}
+		if (this.festa.getIdade().equals("")) {
+			this.festa.setIdade("Sem dado cadastrado");
+		}
+		if (this.festa.getNome().equals("")) {
+			this.festa.setNome("Sem dado cadastrado");
+		}
+
+	}
+	
+	public void excluir() {
+		this.festas.remove(this.festaSelecionado);
 	}
 
 	public void incluir() {
